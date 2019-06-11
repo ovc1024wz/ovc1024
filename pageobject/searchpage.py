081a21fe14e4679fdda4a7d187182c26e63f2576
caoxunmo <958022706@qq.com>
2019-06-11 17:18:52 +0800

    dai ma

--- pageobject/searchpage.py
+++ pageobject/searchpage.py
@@ -1,2 +1,42 @@
 # -*- encoding:utf-8 -*-
 
+from utils.loginpage import LoginPage
+from utils.urls import SEARCH_PAGE
+from coffee.webdrivercoffee import CoffeePage
+
+
+
+
+email = '958022706@qq.com'
+password = '8763366'
+login = LoginPage()
+login.open_and_check()
+login.login(email,password)
+
+
+
+class SeachPage(CoffeePage):
+
+    url = SEARCH_PAGE
+    page_flag_xpath = "//div[@class='c9 mt-3 ml-3 fs12']"
+    page_flag_keyword = u"标题模糊搜索"
+
+    page_flag_search = None
+
+    def input_keywords(self,word):
+        input_keywords = self.driver.find_element_by_xpath("//input[@class='form-control']")
+        input_keywords.send_keys(word)
+        button_login = self.driver.find_element_by_xpath("//button[@type='submit']")
+        button_login.click()
+
+
+    def check_if_search_true(self):
+        output_words = self.driver.find_element_by_xpath("//div[@class='s_top']")
+        print("[DEBUG:%s" % output_words.text)
+        if  output_words.text == self.page_flag_search:
+            return True
+        else:
+            return False
+
+
+
--- utils/urls.py
+++ utils/urls.py
@@ -1,5 +1,9 @@
 # -*-encoding:utf-8-*-
 HOST = "http://47.92.220.226"
 LOGIN_PAGE_URL = HOST + "/bbs/index.php/user/login"
-
+YEWEI_PAGE_URL = HOST + "/bbs/index.php/home/info/id-1"
 SEARCH_PAGE = HOST + "/bbs/index.php/search"
+
+USERS_PAGE_URL = HOST + "/bbs/index.php/user"
+TONGCHENG_PAGE_URL = HOST + "/bbs/index.php/location"
+BEIJING_PAGE_URL = HOST + "/bbs/index.php/location/show/id-1"
