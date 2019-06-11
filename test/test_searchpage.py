7be184576b1525bbcbee65c9fca7a790bff8e40c
caoxunmo <958022706@qq.com>
2019-06-11 17:22:45 +0800

    dai ma

---
+++ test/test_searchpage.py
@@ -0,0 +1,27 @@
+# -*- encoding:utf-8 -*-
+
+import unittest
+from pageobject.searchpage import SeachPage
+
+
+
+
+class TestSearchPage(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.search = SeachPage()
+
+    def test_open_and_check(self):
+        self.assertTrue( self.search.open_and_check())
+
+    def test_search_keywords(self):
+
+        self.page_flag_search = u"获得约9条结果"
+        self.search.input_keywords('11')
+        self.assertTrue(self.search.check_if_search_true())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
