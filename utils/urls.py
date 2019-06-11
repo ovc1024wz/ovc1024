42d0e36f318f107582c66863d7f0a159c485ebc5
wanglong110 <2589240188@qq.com>
2019-06-11 18:50:14 +0800

    王龙

--- utils/urls.py
+++ utils/urls.py
@@ -10,3 +10,13 @@ BEIJING_PAGE_URL = HOST + "/bbs/index.php/location/show/id-1"
 HOME_PAGE = HOST + "/bbs/"
 
 HOME_PAGE_FABIAO = HOST + "/bbs/index.php/weibo/show/id-93"
+
+write_laodao_xpth = "/html/body/div[3]/div/div[1]/div/div/div[1]/form/div/div/button"
+delete_laodao_xpth = "/html/body/div[3]/div/div[1]/div/div/div[3]/ul/li[2]/p/a[2]"
+answerlaodao_tiaozhuan_xpth = "/html/body/div[3]/div/div[1]/div/div/div[3]/ul/li[2]/p/a[1]"
+answer_write_laodao_xpth = "/html/body/div[3]/div/div[1]/div[2]/div[2]/form/textarea"
+answer_write_laodao_click_xpth = "/html/body/div[3]/div/div[1]/div[2]/div[2]/form/div/button"
+laodao_url="http://47.92.220.226/bbs/index.php/weibo"
+laodao_keyword=u"首页"
+laodao_page_flag_xpath="/html/body/div[3]/nav/ol/li[1]/a"
+visit_other_laodao_url="/html/body/div[3]/div/div[2]/div/div[2]/div/ul/li[1]/span[2]/span[3]/a"
