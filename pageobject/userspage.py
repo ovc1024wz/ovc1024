2128de71b49b515be5a15d9f40bae53351acc679
crapblack <763208734@qq.com>
2019-06-11 17:18:43 +0800

    yushiqian

--- pageobject/userspage.py
+++ pageobject/userspage.py
@@ -1,3 +1,9 @@
 # -*-encoding:utf-8-*-
 from coffee.webdrivercoffee import CoffeePage
+from utils.urls import USERS_PAGE_URL
+
+class UsersPage(CoffeePage):
+    url = USERS_PAGE_URL
+    page_flag_xpath = "/html/body/div[3]/div/div[2]/dl/dd/div/span"
+    page_flag_keyword = u"关注数"
 
