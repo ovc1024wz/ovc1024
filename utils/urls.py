aed7108d60d4f697b3845aee3adea45bc1c1b7bf
zhang0522 <15707205332@163.com>
2019-06-11 18:55:32 +0800

    我的社区

--- utils/urls.py
+++ utils/urls.py
@@ -3,6 +3,8 @@ HOST = "http://47.92.220.226"
 LOGIN_PAGE_URL = HOST + "/bbs/index.php/user/login"
 YEWEI_PAGE_URL = HOST + "/bbs/index.php/home/info/id-1"
 SEARCH_PAGE = HOST + "/bbs/index.php/search"
+SHEQU_PAGE_URL = HOST + "/bbs/index.php/my"
+CREATETEAM_PAGE_URL = HOST + "/bbs/index.php/group/create"
 
 USERS_PAGE_URL = HOST + "/bbs/index.php/user"
 TONGCHENG_PAGE_URL = HOST + "/bbs/index.php/location"
